dca9dbdcfa387a443c8b6696b61be147aa3c098b
leo <boisson.leo@gmail.com>
2017-03-10 18:25:06 +0100

    ajout couleur case focus

--- src/plateau.rb
+++ src/plateau.rb
@@ -6,56 +6,77 @@ class Plateau
 	def initialize
 		# Taille de la grille
 		@base = 3
-      		@size = @base*@base
+		@size = @base*@base
 		
 		# Création de la grille
 		@grid = Array.new(@size) do |i|
 			Array.new(@size) do |j| 
-				Case.new(i , j)
+				Case.new(Position.new(i, j))
 			end
 		end
 
       return self
 	end
 
-	# Méthode qui retourne la solution orignale de la case 
-	# @param [Fixnum] posX La position X de la case
-	# @param [Fixnum] posY La position Y de la case
-	# @return (SolutionOriginale)
-	def getCaseOriginale(posX, posY)
-		return @grid[posX][posY].getSolutionOriginale
+	################################################################################
+	#### 								SETTERS									####
+	################################################################################
+
+	# Méthode pour la MAJ de la solution du joueur pour la case
+	# @param [Position] position La position de la case
+	# @return (self)
+	def setCaseJoueur(position, valeur)
+		@grid[position.getX][position.getY].setSolutionJoueur(valeur)
+		return self
+	end
+
+	# OK
+	# Méthode pour la MAJ de la liste des candidats de la case
+	# @param [Position] position La position de la case
+	# @param [Candidat] candidat La liste des candidats de la case
+	def setCaseListeCandidat(position, candidat)
+		@grid[position.getX][position.getY].setListeCandidat(candidat)
+		return self
 	end
 
 	# Méthode pour la MAJ de la solution originale de la case 
-	# @param [Fixnum] posX La position X de la case
-	# @param [Fixnum] posY La position Y de la case
+	# @param [Position] position La position de la case
 	# @param [Fixnum] valeur La valeur de la case
 	# @return (self)
-	def setCaseOriginale(posX, posY, valeur)
-		@grid[posX][posY].setSolutionOriginale(valeur)
+	def setCaseOriginale(position, valeur)
+		@grid[position.getX][position.getY].setSolutionOriginale(valeur)
 		return self
-	end
-	
-	# Méthode qui retourne la solution du joueur pour la case	
-	# @param [Fixnum] posX La position X de la case
-	# @param [Fixnum] posY La position Y de la case
-	# @return (SolutionJoueur)
-	def getCaseJoueur(posX, posY,valeur)
-		return @grid[posX][posY].getSolutionJoueur
 	end
 
-	# Méthode pour la MAJ de la solution du joueur pour la case
-	# @param [Fixnum] posX La position X de la case
-	# @param [Fixnum] posY La position Y de la case
-	# @return (self)
-	def setCaseJoueur(posX, posY, valeur)
-		@grid[posX][posY].setSolutionJoueur(valeur)
-		return self
+	################################################################################
+	#### 								GETTERS									####
+	################################################################################
+	
+	# Méthode qui retourne la solution du joueur pour la case	
+	# @param [Position] position La position de la case
+	# @return (SolutionJoueur)
+	def getCaseJoueur(position)
+		return @grid[position.getX][position.getY].getSolutionJoueur
+	end
+
+	# OK
+	# Méthode qui retourne la liste des candidats pour la case
+	# @param [Position] position La position de la case
+	# @return [Candidat]
+	def getCaseListeCandidat(position)
+		return @grid[position.getX][position.getY].getListeCandidat
+	end
+
+	# Méthode qui retourne la solution orignale de la case 
+	# @param [Position] position La position de la case
+	# @return (SolutionOriginale)
+	def getCaseOriginale(position)
+		return @grid[position.getX][position.getY].getSolutionOriginale
 	end
 
 	# Méthode qui retourne un tableau des valeurs d'une ligne spécifié
 	# @param [Fixnum] ligne La ligne qu'il faut retourner 0-8
-	# @return 
+	# @return [ArrayFixnum]
 	def getLigne(ligne)
 		tableauRetour = Array.new()
 		for n in (0...9)
@@ -66,7 +87,7 @@ class Plateau
 
 	# Méthode qui retourne un tableau des valeurs d'une colonne spécifié
 	# @param [Fixnum] colonne La colonne qu'il faut retourner 0-8
-	# @return 
+	# @return [ArrayFixnum]
 	def getColonne(colonne)
 		tableauRetour = Array.new()
 		for n in (0...9)
@@ -77,7 +98,7 @@ class Plateau
 
 	# Méthode qui retourne un tableau des valeurs d'une region spécifié
 	# @param [Fixnum] region La region qu'il faut retourner 1-9
-	# @return 
+	# @return [ArrayFixnum]
 	def getRegion(region)
 		tableauRetour = Array.new()
 		i = 0
@@ -149,13 +170,40 @@ class Plateau
 		return tableauRetour
 	end
 
+
+	# Méthode qui retourne le numéro de la région en fonction de la position
+	# @param [Position] positon La position de la case
+	# @return [Fixnum]
+	def getRegionNumero(position)
+		if(position.getX <= 2 && position.getY <= 2)
+			region = 1
+		elsif(position.getX <= 2 && position.getY >= 3 && position.getY <= 5)
+			region = 2
+		elsif(position.getX <= 2 && position.getY >= 6)
+			region = 3
+		elsif(position.getX >= 3 && position.getX <= 5 && position.getY <= 2)
+			region = 4
+		elsif(position.getX >= 3 && position.getX <= 5 && position.getY >= 3 && position.getY <= 5)
+			region = 5
+		elsif(position.getX >= 3 && position.getX <= 5 && position.getY >= 6)
+			region = 6
+		elsif(position.getX >= 6 && position.getY <= 2)
+			region = 7
+		elsif(position.getX >= 6 && position.getY >= 3 && position.getY <= 5)
+			region = 8
+		elsif(position.getX >= 6 && position.getY >= 6)
+			region = 9
+		end
+
+		return region
+	end
+
 	# Méthode qui vérifie si un chiffre est sur une ligne
 	# @param chiffre Le chiffre
 	# @param ligne La ligne 
 	# @return boolean
 	def absentLigne(chiffre, ligne)
 		tableauLigne = self.getLigne(ligne)
-		#print tableauLigne
 		return !tableauLigne.include?(chiffre)
 	end
 
@@ -165,7 +213,6 @@ class Plateau
 	# @return boolean
 	def absentColonne(chiffre, colonne)
 		tableauColonne = self.getColonne(colonne)
-		#print tableauColonne
 		return !tableauColonne.include?(chiffre)
 	end
 
@@ -175,50 +222,50 @@ class Plateau
 	# @return boolean
 	def absentRegion(chiffre, region)
 		tableauRegion = self.getRegion(region)
-		#print tableauRegion
 		return !tableauRegion.include?(chiffre)
 	end
 
-	# def candidatPossible(posX, posY)
-	# 	tableauRetour = Array.new(9)
+	# Méthode qui retourne les listes des candidats pour une case
+	# @param [Position] position La position de la case
+	# @return [ArrayFixnum]
+	def candidatPossible(position)
+		tableauRetour = Array.new(9)
 
-	# 	if(posX <=2 && posY<=2)
-	# 		region = 1
-	# 	elsif(posX <= 2 && posY >= 3 && posY <= 5)
-	# 		region = 2
-	# 	elsif(posX <= 2 && posY >= 6)
-	# 		region = 3
-	# 	elsif(posX >= 3 && posX <= 5 && posY <= 2)
-	# 		region = 4
-	# 	elsif(posX >= 3 && posX <= 5 && posY >= 3 && posY <= 5)
-	# 		region = 5
-	# 	elsif(posX >= 3 && posX <= 5 && posY >= 6)
-	# 		region = 6
-	# 	elsif()
-	# 		region = 7
-	# 	elsif()
-	# 		region = 8
-	# 	elsif()
-	# 		region = 9
+		region = self.getRegionNumero(position)
 
-	# 	for i in (0...9)
-	# 		if(absentLigne(i,posX) && absentColonne(i, poY))
-	# 			tableauRetour.insert(i,i)
-	# 		end
-	# 	end
-	# 	return tableauRetour
-	# end
+		for i in (0...9)
+			if(absentLigne(i,position.getX) && absentColonne(i, position.getY) && absentRegion(i, region))
+				tableauRetour.insert(i,i)
+			end
+		end
 
-	# def candidatImpossible(posX, posY)
+		return tableauRetour
+	end
 
-	# end
+
+	# Méthode qui retourne les listes des candidats impossibles pour une case
+	# @param [Position] position La position de la case
+	# @return [ArrayFixnum]
+	def candidatImpossible(position)
+		tableauRetour = Array.new(9)
+
+		region = self.getRegionNumero(position)
+
+		for i in (0...9)
+			if(!absentLigne(i,position.getX) && !absentColonne(i, position.getY) && !absentRegion(i, region))
+				tableauRetour.insert(i,i)
+			end
+		end
+
+		return tableauRetour
+	end
 
 	# Méthode pour générer une grille complete aléatoirement
 	def completeGrille
 		pattern = Array.new(@size){|i| i+1}.shuffle
 		@size.times do |y|
 			@size.times do |x|
-      			setCaseOriginale(x, y, pattern[x])
+      			setCaseOriginale(Position.new(x, y), pattern[x])
     		end
     		@base.times{|i| pattern.push pattern.shift}
     		pattern.push pattern.shift if @base - (y % @base) == 1
@@ -254,17 +301,3 @@ class Plateau
       return res
 	end
 end
-
-plateau = Plateau.new()
-
-plateau.completeGrille()
-
-#plateau.setCaseJoueur(1,0,1)
-#plateau.setCaseJoueur(8,8,2)
-
-
-print "\n", plateau
-
-puts plateau.absentRegion(1,1)
-puts plateau.absentLigne(1,1)
-puts plateau.absentColonne(1,0)
