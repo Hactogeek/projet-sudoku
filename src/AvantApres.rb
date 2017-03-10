e59510be8a4fc50f7374b174e03f4cd8aab1edb5
Rémi <remibouvet72@gmail.com>
2017-03-10 09:35:19 +0100

    Ajout des commentaires

--- src/AvantApres.rb
+++ src/AvantApres.rb
@@ -1,10 +1,13 @@
 class AvantApres
-
-	#@avant
-	#@apres
+	# Classe qui permet de stocker deux elements
+	# @avant
+	# @apres
 
 	private_class_method :new
 
+	# Constucteur de la classe
+	# @param avant : l'etat precedant
+	# @param apres : l'etat suivant
 	def AvantApres.creer(avant, apres)
 		new(avant, apres)
 	end
@@ -12,6 +15,7 @@ class AvantApres
 	def initialize(avant, apres)
 		@avant = avant;
 		@apres = apres;
+		return self
 	end
 		
 end
--- src/GestionMemento.rb
+++ src/GestionMemento.rb
@@ -5,6 +5,8 @@ class GestionMemento
 
 	private_class_method :new
 
+	# Constructeur de la classe
+	# @param : plateau	
 	def GestionAction.creer(plateau)
 		new(plateau)
 	end
@@ -13,37 +15,52 @@ class GestionMemento
 		@undos = Array.new
 		@redos = Array.new
 		@plateau = plateau
+		return self
 	end
 
+	# Méthode qui retourne vrai si on peut undo, sinon faux
+	# @return : vrai ou faux
 	def canUndo?
 		return @undos.empty? == false
 	end
 
+	# Méthode qui retourne vrai si on peut redo, sinon faux
+	# @return : vrai ou faux
 	def canRedo?
 		return @redos.empty? == false
 	end
 
+	# Méthode qui permet d'executer une action placée en paramètre, elle gère également la sauvegarde de l'état precedant et suivant l'action dans le memento.
+	# @param action : action a executer
+	# @return : self
 	def exec(action)
 		avant = plateau.etatCourant
 		action.exec
 		apres = plateau.etatCourant
 		@undos.push(AvantApres.creer(avant, apres))
 		@redos.clear
+		return self
 	end
 
+	# Méthode qui permet de revenir à l'état précédant la dernière action
+	# @return : self
 	def undo
 		if @undos.empty? == false
 			dernierMemento = @undos.pop
 			dernierMemento.avant.restaurer
 			@redos.push(dernierMemento)
 		end
+		return self
 	end
 
+	# Méthode qui permet de revenir à l'état suivant la dernière action
+	# @return : self
 	def redo
 		if @redos.empty? == false
 			dernierMemento = @redos.pop
 			dernierMemento.apres.restaurer
 			@undos.push(dernierMemento)
 		end
+		return self
 	end
 end
