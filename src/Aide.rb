976a92a5cf7f49bbb6b8a18759fd17b098d1e4eb
modira mok <spi3142@ic2s112-08.univ-lemans.fr>
2017-03-23 14:33:34 +0100

    Ajout du squelle de la classe Aide

---
+++ src/Aide.rb
@@ -0,0 +1,53 @@
+require './plateau'
+
+class Aide
+	def Aide.creer()
+		new()
+	end
+
+	def initialize()
+		@plateau=Plateau.new
+	end
+
+	#Place tous les candidats de la case
+	def candidatPossible(pos)
+
+	end
+
+	#Place tous les candidats de chaque case du plateau
+	def candidatGrille()
+
+	end
+
+	#Resoud la grille
+	def resoudre()
+
+	end
+
+	#Le joueur indique en paramètre le symbole candidat de la position
+	#si c'est possible, on retourne true. Sinon, on retourne
+	#la position du symbole qui indique l'impossibilité de placer le symbole candidat
+	def explicationChoix(candidat, pos)
+
+	end
+
+	#Indique si la case à cette position est valide
+	def validerCase(pos)
+
+	end
+
+	#Indique la position du coup suivant à jouer
+	def coupSuivant()
+
+	end
+
+	#Affiche les méthodes de résolution
+	def afficherLesMethodesDeResolution
+
+	end
+
+	#Retourne la liste des cases qui ont un candidat unique
+	def candidatsUniques()
+
+	end
+end
