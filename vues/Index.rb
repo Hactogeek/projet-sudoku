35fba0286570bfd8fe4014d6e0ce0779490a62c6
Hactogeek <tony.marteau@outlook.com>
2017-04-04 14:21:11 +0200

    Commit pour pull

--- vues/Index.rb
+++ vues/Index.rb
@@ -15,7 +15,7 @@ class Index < WindowSudoku
 		#Redirection des boutons
 		seConnecter.signal_connect "clicked" do |widget|
 			hide
-			Dir.chdir(Dir.pwd+"./profil")
+			Dir.chdir(Dir.pwd+"/profil")
 			newWindow=Connexion.new
 		end
 		creerProfil.signal_connect "clicked" do |widget|
