c820c5b14345e6dd671f8912706745c498b4ea59
Hactogeek <tony.marteau@outlook.com>
2017-04-04 13:50:16 +0200

    Méthode de résolution singleBox

--- api/Aide.rb
+++ api/Aide.rb
@@ -92,7 +92,7 @@ class Aide
 						add = (@partie.getPlateau().getLigne(x+1).include?(n)) || (@partie.getPlateau().getLigne(x+2).include?(n))
 					end
 
-					break if add = false
+					break if add == false
 
 					if x%3 == 2
 						add = (@partie.getPlateau().getColonne(x-1).include?(n)) || (@partie.getPlateau().getColonne(x-2).include?(n))
@@ -104,6 +104,53 @@ class Aide
 
 					if add 
 						listeCase.push(Position.new(x,y))
+					end
+				end
+			end
+		end
+		return listeCase
+	end
+	
+		# Retourne une liste de case
+	def singleBox()
+		listeCase = Array.new
+		@partie.getPlateau().each do |x,y,laCase|
+			if (laCase.getSolutionJoueur() == nil)
+				for candidat in laCase.getCandidat().getListeCandidat
+					add = true
+					ligne = true
+					colonne = true
+
+					posX = posX-(x%3)
+					posY = posY-(y%3)
+
+					for n in (posX...posX+3)
+						for m in (posY...posY+3)
+							if @partie.getPlateau.getCase(Position.new(n,m)).getSolutionJoueur() == nil
+								if n == x
+									if ligne 
+										ligne = @partie.getPlateau.getCase(Position.new(n,m)).getCandidat.include?(candidat)
+									end
+								elsif m == y
+									if colonne
+										colonne = @partie.getPlateau.getCase(Position.new(n,m)).getCandidat.include?(candidat)
+									end
+								else
+									add = !(@partie.getPlateau.getCase(Position.new(n,m)).getCandidat.include?(candidat))
+								end
+							end
+							break if add == false || (ligne == false && colonne == false)
+						end
+						break if add == false || (ligne == false && colonne == false)
+					end
+
+					if add 
+						if ligne
+
+						elsif colonne
+
+						end
+						# Supprimer le candidat des autres cases de la ligne ou de la colonne
 					end
 				end
 			end
