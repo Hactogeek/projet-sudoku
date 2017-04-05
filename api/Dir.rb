13590132de4cbe45016f2beef03d6e56cc4fcf02
PNalex <lelexpn@gmail.com>
2017-04-05 10:04:13 +0200

    maj commentaire pour doc

--- api/Dir.rb
+++ api/Dir.rb
@@ -4,23 +4,14 @@
 # * *Heritage*	: Aucun lien
 #
 class Dir
-
-
-	# * *Description*:
-	# 
 	# méthode de création d'un repertoire(profil)
 	#
-	# * *Paramètre*:
-	#
-	# - +unPseudo+ -> pseudo du joueur 
-	#
-	# * *Exemple*:
-	#
-	# unDossier=Dir.creer("Yassine")
-	#
+	# * [Paramètre :]
+	# 				un pseudo	 
 	def Dir.creer(unPseudo)
 		new(unPseudo)
 	end 	
+
 	def initialize(unPseudo)
 		#Dir.chdir("/Users/mac/Documents/projet-sudoku/projet-sudoku/src/profil")
 		Dir.chdir("profil")
@@ -29,33 +20,17 @@ class Dir
 		end
 		Dir.chdir("../")
 	end 
-	# * *Description*:
-	# 
+
 	# méthode qui permet de se mettre dans un repertoire(profil) 
-	#
-	# * *Paramètre*:
-	#
-	# - +unPseudo+ -> pseudo du joueur 
-	#
-	# * *Exemple*:
-	#
-	# dossier.semettreDansProfil("Yassine")
-	#
+	# * [Paramètre :]
+	# 				pseudo
 	def semettreDansProfil(unPseudo)
 		Dir.chdir("profil/#{unPseudo}")
 	end 
-	# * *Description*:
-	# 
+
 	# méthode qui permet de supprimer un repertoire (profil)
-	#
-	# * *Paramètre*:
-	#
-	# - +unPseudo+ -> pseudo du joueur 
-	#
-	# * *Exemple*:
-	#
-	# profil.supprimerProfil("Yassine")
-	#
+	# * [Paramètre :]
+	# 				pseudo	
 	def supprimerProfil(unPseudo)
 		Dir.chdir("profil")
 		Dir.rmdir(unPseudo)
