527bfcf85b0e4ea889d7901b51e4e52651f51baa
Hideky <soris-orami@live.fr>
2017-04-06 16:38:41 +0200

    Amélioration de l'interface de Paramètres :panda_face:

--- vues/Parametres.rb
+++ vues/Parametres.rb
@@ -19,14 +19,12 @@ class Parametres < Gtk::Window
 		@partie = partie
 
 		# Property
-		set_title "ku"
+		set_title "Paramètres"
 		set_window_position(Gtk::WindowPosition::CENTER)
 		set_resizable(false)
-		vbox = Gtk::Box.new(:vertical, 0)
-		hbox = Gtk::Box.new(:horizontal, 0)
 		
-		vbox.add(Gtk::Label.new("Choisissez votre thème:"))
-		vbox.add(hbox)
+		table = Gtk::Table.new(10,2)
+		table.attach(Gtk::Label.new().set_markup("<span font-weight=\"bold\">Choisissez votre thème:</span>"), 0,2 ,0 ,1)
 		
 		colorFocus = @partie.getPreferences().colorFocus
 		colorEquals =  @partie.getPreferences().colorEquals
@@ -50,49 +48,57 @@ class Parametres < Gtk::Window
 		colorButtonFocus.signal_connect("color-set"){
 			colorFocus = colorButtonFocus.color.to_s[0..2] + colorButtonFocus.color.to_s[5..6] + colorButtonFocus.color.to_s[9..10]
 		}
-		vbox.add(Gtk::Box.new(:horizontal, 0).add(labelFocus).add(colorButtonFocus))
+		table.attach(labelFocus, 0,1,1,2)
+		table.attach(colorButtonFocus, 1,2,1,2)
 
 		colorButtonEquals = Gtk::ColorButton.new(Gdk::Color.parse(colorEquals))
 		colorButtonEquals.signal_connect("color-set"){
 			colorEquals = colorButtonEquals.color.to_s[0..2] + colorButtonEquals.color.to_s[5..6] + colorButtonEquals.color.to_s[9..10]
 		}
-		vbox.add(Gtk::Box.new(:horizontal, 0).add(labelEquals).add(colorButtonEquals))
+		table.attach(labelEquals, 0,1,2,3)
+		table.attach(colorButtonEquals, 1,2,2,3)
 
 		colorButtonAide = Gtk::ColorButton.new(Gdk::Color.parse(colorAide))
 		colorButtonAide.signal_connect("color-set"){
 			colorAide = colorButtonAide.color.to_s[0..2] + colorButtonAide.color.to_s[5..6] + colorButtonAide.color.to_s[9..10]
 		}
-		vbox.add(Gtk::Box.new(:horizontal, 0).add(labelAide).add(colorButtonAide))
+		table.attach(labelAide, 0,1,3,4)
+		table.attach(colorButtonAide, 1,2,3,4)
 
 		colorButtonError = Gtk::ColorButton.new(Gdk::Color.parse(colorError))
 		colorButtonError.signal_connect("color-set"){
 			colorError = colorButtonError.color.to_s[0..2] + colorButtonError.color.to_s[5..6] + colorButtonError.color.to_s[9..10]
 		}
-		vbox.add(Gtk::Box.new(:horizontal, 0).add(labelError).add(colorButtonError))
+		table.attach(labelError, 0,1,4,5)
+		table.attach(colorButtonError, 1,2,4,5)
 
 		colorButtonNeutral = Gtk::ColorButton.new(Gdk::Color.parse(colorNeutral))
 		colorButtonNeutral.signal_connect("color-set"){
 			colorNeutral = colorButtonNeutral.color.to_s[0..2] + colorButtonNeutral.color.to_s[5..6] + colorButtonNeutral.color.to_s[9..10]
 		}
-		vbox.add(Gtk::Box.new(:horizontal, 0).add(labelNeutral).add(colorButtonNeutral))
+		table.attach(labelNeutral, 0,1,5,6)
+		table.attach(colorButtonNeutral, 1,2,5,6)
 
 		colorButtonTextOriginal = Gtk::ColorButton.new(Gdk::Color.parse(colorTextOriginal))
 		colorButtonTextOriginal.signal_connect("color-set"){
 			colorTextOriginal = colorButtonTextOriginal.color.to_s[0..2] + colorButtonTextOriginal.color.to_s[5..6] + colorButtonTextOriginal.color.to_s[9..10]
 		}
-		vbox.add(Gtk::Box.new(:horizontal, 0).add(labelTextOriginal).add(colorButtonTextOriginal))
+		table.attach(labelTextOriginal, 0,1,6,7)
+		table.attach(colorButtonTextOriginal, 1,2,6,7)
 
 		colorButtonTextPlayer = Gtk::ColorButton.new(Gdk::Color.parse(colorTextPlayer))
 		colorButtonTextPlayer.signal_connect("color-set"){
 			colorTextPlayer = colorButtonTextPlayer.color.to_s[0..2] + colorButtonTextPlayer.color.to_s[5..6] + colorButtonTextPlayer.color.to_s[9..10]
 		}
-		vbox.add(Gtk::Box.new(:horizontal, 0).add(labelTextPlayer).add(colorButtonTextPlayer))
+		table.attach(labelTextPlayer, 0,1,7,8)
+		table.attach(colorButtonTextPlayer, 1,2,7,8)
 
 		colorButtonTextCandidat = Gtk::ColorButton.new(Gdk::Color.parse(colorTextCandidat))
 		colorButtonTextCandidat.signal_connect("color-set"){
 			colorTextCandidat = colorButtonTextCandidat.color.to_s[0..2] + colorButtonTextCandidat.color.to_s[5..6] + colorButtonTextCandidat.color.to_s[9..10]
 		}
-		vbox.add(Gtk::Box.new(:horizontal, 0).add(labelTextCandidat).add(colorButtonTextCandidat))
+		table.attach(labelTextCandidat, 0,1,8,9)
+		table.attach(colorButtonTextCandidat, 1,2,8,9)
 
 
 		buttonSave = Gtk::Button.new(:label =>"Sauvergarder", :use_underline => nil, :stock_id => nil)
@@ -105,8 +111,8 @@ class Parametres < Gtk::Window
 			@sousGrille.setColorCandidat(colorTextCandidat)
 			hide()
 		}
-		vbox.add(buttonSave)		
-		add(vbox)
+		table.attach(buttonSave, 0,2,9,10)
+		add(table)
 		show_all
 	end
 end
